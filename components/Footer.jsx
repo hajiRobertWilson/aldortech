db969152b6d460173e9948a6d2306a788ed72660
hajiMushtaqtemp <aliahmadofficial884@gmail.com>
2024-07-28 11:28:10 +0000

    Under Development

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,8 +1,9 @@
 export default function Footer() {
-    return (
-      <>
-      <h1>Footer</h1>
-      </>
-    );
-  }
-  
+  return (
+    <>
+      <div style={{height:'400px',width:'100%',background:'linear-gradient(rgba(0, 0, 0, 1), rgba(0, 0, 0, 0.5))',color:'wheat'}}>
+        <h1>Footer</h1>
+      </div>
+    </>
+  );
+}
