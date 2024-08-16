64e52724442b9a965c7273c92e4a428fdd9b099f
hajiMushtaqtemp <aliahmadofficial884@gmail.com>
2024-08-16 05:29:18 +0000

    Under Development

--- app/api/blog/readBlog/route.js
+++ app/api/blog/readBlog/route.js
@@ -2,8 +2,10 @@ import { MongoClient, ObjectId } from "mongodb";
 
 const dbClient = new MongoClient(process.env.URI_String);
 
-export async function GET(req) {
+export async function POST(req) {
     try {
+        let body = await req.json()
+        console.log(body)
         console.log('Request Recieved by ReadBlog route')
         await dbClient.connect();
         const cards = [];
@@ -14,10 +16,16 @@ export async function GET(req) {
             cards.push(key)
         }
         const clipCollection = dataBase.collection('ClipBoard')
-        const find = await clipCollection.findOne()
-        console.log(find)
-        let resData = cards.find(card => card.id === find.blogId)
-        return new Response(JSON.stringify({ msg: 'Request Recieved', data: resData, blogId: `BlogID from readBlog route: ${find.blogId}` }), {
+        const find = await clipCollection.findOne({ blogId: body })
+        if (find.blogId === body) {
+            console.log(find)
+            let resData = cards.find(card => card.id === find.blogId)
+            return new Response(JSON.stringify({ msg: 'Request Recieved', data: resData, blogId: `BlogID from readBlog route: ${body}` }), {
+                status: 200,
+                headers: { 'Content-Type': 'application/json' }
+            })
+        }
+        return new Response(JSON.stringify({ msg: 'Request Not valid', blogId: `BlogID from readBlog route: ${body}` }), {
             status: 200,
             headers: { 'Content-Type': 'application/json' }
         })
